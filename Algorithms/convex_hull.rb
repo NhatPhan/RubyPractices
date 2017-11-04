d4bc5f4703d823a0a4e5b2e1e6e4f23478b5135f
NhatPhan <minhnhat.cs.phan@gmail.com>
2017-11-04 21:27:43 +0800

    Add quick hull solution to convex hull problem

--- Algorithms/convex_hull.rb
+++ Algorithms/convex_hull.rb
@@ -1,6 +1,7 @@
 require './cartesian_points_helper'
 require 'benchmark'
 require 'matrix'
+require 'byebug'
 require 'set'
 
 class ConvexHull
@@ -14,7 +15,7 @@ class ConvexHull
     setup until points_requirement_satisfied?
   end
 
-  def re_setup
+  def reset
     @points = nil
     setup until points_requirement_satisfied?
   end
@@ -42,7 +43,7 @@ class ConvexHull
   end
 
   def graham_scan
-    points.sort!{ |a,b| a[1] <=> b[1] }
+    points.sort_by!{ |point| point[1] }
 
     angles = points[1..-1].map { |point_b| [angle_with_line(points[0], point_b), point_b] }
     angles.sort_by!{ |angle| angle[0] }
@@ -64,14 +65,43 @@ class ConvexHull
     result
   end
 
+  def quick_hull
+    points.sort_by!{ |point| point[1] }
+    low_high = vector(points[0], points[-1])
+
+    left_points = points[1..-2].select do |point|
+      determinant(low_high, vector(points[0], point)) > 0
+    end
+    right_points = points[1..-2].select do |point|
+      determinant(low_high, vector(points[0], point)) < 0
+    end
+
+    result = [points[0], points[-1]] +
+             find_hull(left_points, points[0], points[-1]) +
+             find_hull(right_points, points[-1], points[0])
+
+    result
+  end
+
   def benchmarks
     brute_force_time  = Benchmark.measure { brute_force  }
+
+    # Worst case sort when algorithm run
+    points.sort_by!{ |point| !point[1] }
     jarvis_march_time = Benchmark.measure { jarvis_march }
+
+    # Worst case sort when algorithm run
+    points.sort_by!{ |point| !point[1] }
     graham_scan_time  = Benchmark.measure { graham_scan  }
+
+    # Worst case sort when algorithm run
+    points.sort_by!{ |point| !point[1] }
+    quick_hull_time  = Benchmark.measure { quick_hull  }
 
     puts "Time taken brute forces:  #{brute_force_time.real}"
     puts "Time taken jarvis_march:  #{jarvis_march_time.real}"
     puts "Time taken graham_scan :  #{graham_scan_time.real}"
+    puts "Time taken quick_hull  :  #{quick_hull_time.real}"
   end
 
   private
@@ -148,6 +178,59 @@ class ConvexHull
 
   def angle_with_line(point_a, point_b, d = [1, 0])
     vector_ab = vector(point_a, point_b)
+    return 0 if vector_ab == [0,0]
     Vector.elements(d).angle_with(Vector.elements(vector_ab))
+  end
+
+  def dot_product(point_a, point_b)
+    Vector.elements(point_a).dot Vector.elements(point_b)
+  end
+
+  def find_hull(points, point_a, point_b)
+    return [] if points.empty?
+
+    vector_ab = vector(point_a, point_b)
+    sorted_points = points.sort_by { |point_p| determinant(vector_ab, vector(point_a, point_p)).abs }
+
+    point_c   = sorted_points.pop
+    vector_ac = vector(point_a, point_c)
+    vector_bc = vector(point_b, point_c)
+
+    left_points = sorted_points.select do |point|
+      determinant(vector_ac, vector(point_a, point)) > 0
+    end
+    right_points = sorted_points.select do |point|
+      determinant(vector_bc, vector(point_b, point)) < 0
+    end
+
+    result = [point_c] +
+             find_hull(left_points, point_a, point_c) +
+             find_hull(right_points, point_c, point_b)
+
+    result
+  end
+
+  # Check if point P is in triangle ABC using barycentric coordinates
+  # Read: http://blackpawn.com/texts/pointinpoly/
+  def point_in_triangle?(point_p, point_a, point_b, point_c)
+    # Compute vectors
+    v0 = vector(point_c, point_a)
+    v1 = vector(point_b, point_a)
+    v2 = vector(point_p, point_a)
+
+    # Compute dot products
+    dot00 = dot_product(v0, v0)
+    dot01 = dot_product(v0, v1)
+    dot02 = dot_product(v0, v2)
+    dot11 = dot_product(v1, v1)
+    dot12 = dot_product(v1, v2)
+
+    # Compute barycentric coordinates
+    inverse_denominator = 1 / (dot00 * dot11 - dot01 * dot01)
+    u = (dot11 * dot02 - dot01 * dot12) * inverse_denominator
+    v = (dot00 * dot12 - dot01 * dot02) * inverse_denominator
+
+    # Check if point is in triangle
+    return (u >= 0) && (v >= 0) && (u + v < 1)
   end
 end
